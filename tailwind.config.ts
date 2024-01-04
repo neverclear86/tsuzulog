d83706859cdf96bc75174eb90191a5a0aec192b2
LinaTsukusu <lina.tsukusu@ikili.pro>
2024-01-04 22:14:50 +0900

    create project

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { tzlg_theme } from './src/tzlg_theme'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+				],
+				custom: [
+					tzlg_theme,
+				],
+			},
+		}),
+	],
+} satisfies Config;
