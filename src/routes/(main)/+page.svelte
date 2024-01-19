fa6f15aa9dc43a166daa362f3c78b886953352bc
LinaTsukusu <lina.tsukusu@ikili.pro>
2024-01-19 22:00:14 +0900

    再フォーマットとactions設定

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import Editor from "$lib/components/editor/Editor.svelte"
+  import Editor from "$lib/components/editor/Editor.svelte"
 
-	let markdown = ""
+  let markdown = ""
 </script>
 
 <div class="flex justify-center items-center gap-4 p-4 h-dvh">
-	<div class="basis-full 2xl:basis-1/2 h-full overflow-auto overscroll-none border-2">
-		<Editor bind:markdown></Editor>
-	</div>
-	<div class="hidden lg:block basis-1/2"></div>
+  <div class="basis-full 2xl:basis-1/2 h-full overflow-auto overscroll-none border-2">
+    <Editor bind:markdown></Editor>
+  </div>
+  <div class="hidden lg:block basis-1/2"></div>
 </div>
